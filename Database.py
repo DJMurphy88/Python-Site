5b8d2f9ea7e1b7d5ca04c38c7ac3a655cdd8988b
Denis Murphy <djmurphy2001@gmail.com>
2021-11-15 16:08:21 -0330

    First versions of Database and Objects modules

---
+++ Database.py
@@ -0,0 +1,52 @@
+import sqlite3
+from contextlib import closing
+
+from Objects import Game
+
+DBFILE = "game_db.sqlite"
+conn = None
+
+def connect():
+    global conn
+    conn = sqlite3.connect(DBFILE)
+    conn.row_factory = sqlite3.Row
+
+def close():
+    if conn:
+        conn.close()
+
+def make_game(row):
+    return Game(row["title"], row["image"], row["system"],
+                row["release_date"], row["genre"], row["complete"])
+
+def get_game():
+    query = '''SELECT title, image, system, release_date, genre, complete FROM Games'''
+    with closing(conn.cursor()) as c:
+        c.execute(query)
+        results = c.fetchall()
+
+    games = []
+    for row in results:
+        games.append(make_game(row))
+    return games
+
+def add_game(game):
+    query = '''INSERT INTO Games (title, image, system, release_date, genre, complete)'''
+    with closing(conn.cursor()) as c:
+        c.execute(query, game.getTitle(), game.getImage(), game.getSystem(),
+                  game.getDate(), game.getGenre(), game.getComplete())
+        conn.commit()
+
+def update_game(title, image, system, date, genre, complete):
+    query = '''UPDATE Games SET title = ?, image = ?, system = ?, 
+                release_date = ?, genre = ?, complete = ?'''
+
+    with closing(conn.cursor()) as c:
+        c.execute(query, (title, image, system, date, genre, complete))
+        conn.commit()
+
+def delete_game(title):
+    query = '''DELETE FROM Games WHERE title = ?'''
+    with closing(conn.cursor()) as c:
+        c.execute(query, (title,))
+        conn.commit()
