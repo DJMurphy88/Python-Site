76b886282684b16da203a0be4e4c3d58e9fa7d5b
Denis Murphy <djmurphy2001@gmail.com>
2021-11-16 17:55:43 -0330

    First versions of Objects.py and Database.py

--- Objects.py
+++ Objects.py
@@ -1,2 +1,26 @@
-class game:
-    def
+class Game:
+    def __init__(self, title, image, system, date, genre, complete):
+        self.__tile__ = title
+        self.__image__ = image
+        self.__system__ = system
+        self.__date__ = date
+        self.__genre__ = genre
+        self.__complete__ = complete
+
+    def getTitle(self):
+        return self.__tile__
+
+    def getImage(self):
+        return self.__image__
+
+    def getSystem(self):
+        return self.__system__
+
+    def getDate(self):
+        return self.__date__
+
+    def getGenre(self):
+        return self.__genre__
+
+    def getComplete(self):
+        return self.__complete__
